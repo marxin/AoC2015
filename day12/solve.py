7e3bbca7f1f378cd5c2217018e3b70e157905e82
marxin <mliska@suse.cz>
2020-12-14 20:25:39 +0100

    Finish day 12, part 1.

---
+++ day12/solve.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import json
+
+data = json.load(open('input.txt'))
+
+total = 0
+
+def countit(data):
+    global total
+    if isinstance(data, dict):
+        for k, v in data.items():
+            assert not k.isdigit()
+            countit(v)
+    elif isinstance(data, list):
+        for x in data:
+            countit(x)
+    elif isinstance(data, int):
+        total += data
+
+countit(data)
+print(total)
