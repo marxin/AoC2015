9a1754a4b04b7ee268b2d79e2719a40aa79c0a4f
marxin <mliska@suse.cz>
2020-12-28 17:48:26 +0100

    Finish part 2.

--- day20/solve.py
+++ day20/solve.py
@@ -6,13 +6,13 @@ N = 2000000
 a = [0] * N
 
 for i in range(1, N):
-    s = i
-    while s < N:
-        a[s] += 10 * i
-        s += i
+    for j in range(1, 51):
+        index = i * j
+        if index < N:
+            a[index] += 11 * i
 
-print(a[N - 1])
 print(minimum)
+print(a[N - 1])
 
 for i in range(1, N):
     if a[i] >= minimum:
